98979eada014a7ee781f8f20932b7ab9e7dab8ab
enieves <enieves@106-29.info2>
2022-09-21 13:25:27 +0200

    hi

---
+++ Testgit/src/testgit/Testgit.java
@@ -0,0 +1,12 @@
+package testgit;
+
+import java.util.Scanner;
+
+public class Testgit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("a");
+	}
+
+}
